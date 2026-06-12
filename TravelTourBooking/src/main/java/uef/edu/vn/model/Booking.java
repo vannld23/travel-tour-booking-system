7759db2b08b3fa27774840284b1155b1832b425c
DuyKhoi282 <tranquocduykhoi@gmail.com>
2026-06-13 01:34:32 +0700

    tạo model booking.java

--- TravelTourBooking/src/main/java/uef/edu/vn/model/Booking.java
+++ TravelTourBooking/src/main/java/uef/edu/vn/model/Booking.java
@@ -4,10 +4,74 @@
  */
 package uef.edu.vn.model;
 
-/**
- *
- * @author LENOVO
- */
+import java.sql.Timestamp;
+
 public class Booking {
-    
+
+    private int bookingId;
+    private int userId;
+    private int tourId;
+    private Timestamp bookingDate;
+    private int numberOfPeople;
+    private double totalPrice;
+    private String bookingStatus;
+
+    public Booking() {
+    }
+
+    public int getBookingId() {
+        return bookingId;
+    }
+
+    public void setBookingId(int bookingId) {
+        this.bookingId = bookingId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getTourId() {
+        return tourId;
+    }
+
+    public void setTourId(int tourId) {
+        this.tourId = tourId;
+    }
+
+    public Timestamp getBookingDate() {
+        return bookingDate;
+    }
+
+    public void setBookingDate(Timestamp bookingDate) {
+        this.bookingDate = bookingDate;
+    }
+
+    public int getNumberOfPeople() {
+        return numberOfPeople;
+    }
+
+    public void setNumberOfPeople(int numberOfPeople) {
+        this.numberOfPeople = numberOfPeople;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public String getBookingStatus() {
+        return bookingStatus;
+    }
+
+    public void setBookingStatus(String bookingStatus) {
+        this.bookingStatus = bookingStatus;
+    }
 }
