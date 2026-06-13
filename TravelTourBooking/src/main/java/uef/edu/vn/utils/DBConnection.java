cb259c013a958f04d2498df1ed7382e4db5c70b3
Hậu Cò <10a9nguyentranconghau@gmail.com>
2026-06-13 12:30:42 +0700

    đổi

--- TravelTourBooking/src/main/java/uef/edu/vn/utils/DBConnection.java
+++ TravelTourBooking/src/main/java/uef/edu/vn/utils/DBConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DBConnection {
 
     private static final String URL =
-            "jdbc:mysql://localhost:3306/travel_tour_booking";
+            "jdbc:mysql://localhost:3307/travel_tour_booking";
 
     private static final String USER = "root";
 
