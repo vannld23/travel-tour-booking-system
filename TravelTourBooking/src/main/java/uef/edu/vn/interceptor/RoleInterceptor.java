c236665a2b3d6858ca9cf1e0262c2be5fdfa4ae3
Hậu Cò <10a9nguyentranconghau@gmail.com>
2026-06-29 09:02:34 +0700

    Create RoleInterceptor.java

---
+++ TravelTourBooking/src/main/java/uef/edu/vn/interceptor/RoleInterceptor.java
@@ -0,0 +1,29 @@
+package uef.edu.vn.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import uef.edu.vn.model.User;
+
+public class RoleInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        HttpSession session = request.getSession(false);
+        User currentUser = (session != null) ? (User) session.getAttribute("currentUser") : null;
+
+        if (currentUser == null) {
+            response.sendRedirect(request.getContextPath() + "/auth/login");
+            return false;
+        }
+
+        if (currentUser.getRoleId() != 1) {
+            response.sendRedirect(request.getContextPath() + "/user/profile");
+            return false;
+        }
+
+        return true;
+    }
+}
