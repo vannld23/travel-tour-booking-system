59956226b12c7bb9f5e7b413cab9ba18a15692b2
Hậu Cò <10a9nguyentranconghau@gmail.com>
2026-07-02 12:41:52 +0700

    fix: Resolve ambiguous 'Row' class compile error in ExportService by using explicit imports

--- TravelTourBooking/src/main/java/uef/edu/vn/service/ExportService.java
+++ TravelTourBooking/src/main/java/uef/edu/vn/service/ExportService.java
@@ -1,11 +1,31 @@
 package uef.edu.vn.service;
 
-import com.lowagie.text.*;
+import com.lowagie.text.Document;
+import com.lowagie.text.Paragraph;
+import com.lowagie.text.Chunk;
+import com.lowagie.text.Phrase;
+import com.lowagie.text.PageSize;
+import com.lowagie.text.Rectangle;
 import com.lowagie.text.Font;
-import com.lowagie.text.pdf.*;
-import org.apache.poi.ss.usermodel.*;
+import com.lowagie.text.FontFactory;
+import com.lowagie.text.Element;
+import com.lowagie.text.pdf.PdfPTable;
+import com.lowagie.text.pdf.PdfPCell;
+import com.lowagie.text.pdf.PdfWriter;
+import com.lowagie.text.pdf.draw.LineSeparator;
+
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.IndexedColors;
+import org.apache.poi.ss.usermodel.FillPatternType;
+import org.apache.poi.ss.usermodel.BorderStyle;
+import org.apache.poi.ss.usermodel.HorizontalAlignment;
+import org.apache.poi.ss.usermodel.VerticalAlignment;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 import uef.edu.vn.dao.BookingDAO;
 import uef.edu.vn.dao.PaymentDAO;
 import uef.edu.vn.model.Booking;
